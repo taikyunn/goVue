df8a772930f50c2e9670fd643d3e3a21dd4b5f12
taikyunn <taichi_141@me.com>
2021-11-04 08:18:26 +0900

    Initial commit

---
+++ assets/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <List></List>
+  </div>
+</template>
+
+<script>
+// import axios from 'axios'
+// Vue.prototype.$axios = axios
+import List from './components/List.vue'
+// 次にやるのはこのコンポーネントの値を下のaxiosを利用してうまくGo側に渡す方法を考える
+// axiosでGo側にデータを送るのはApp.vue?それともList.vue??
+export default {
+  components: {
+    List
+  },
+  // created() {
+  //   this.$axios.get('http://localhost/api/')
+  //   .then(response => {
+  //     console.log(response)
+  //   })
+  // }
+
+}
+</script>
+
---
+++ assets/src/components/List.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <h2>TodoList</h2>
+    <form v-on:submit.prevent>
+      <input type="text" v-model="newItem">
+      <button v-on:click="addItem">Add</button>
+    </form>
+    <pre>{{ $data }}</pre>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newItem: '',
+      todos:[],
+      string: [],
+    }
+  },
+  methods: {
+    addItem: function() {
+      if (this.newItem === '') {
+        return
+      }
+      var todo = {
+        item: this.newItem
+      }
+      console.log(todo)
+      this.todos.push(todo)
+      this.newItem = ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+  div {
+    color:  red;
+  }
+</style>
